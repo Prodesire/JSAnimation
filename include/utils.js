aa330be1bfef34ea38902e48e40f19f88ad7d084
Prodesire <Prodesire@yeah.net>
2017-01-17 23:43:58 +0800

    Implement Chapter4 example codes

--- include/utils.js
+++ include/utils.js
@@ -77,3 +77,21 @@ utils.parseColor = function (color, toNumber) {
         return color;
     }
 };
+
+utils.colorToRGB = function (color, alpha) {
+    if (typeof color === 'string' && color[0] === '#') {
+        color = window.parseInt(color.slice(1), 16);
+    }
+    alpha = (alpha === undefined) ? 1 : alpha;
+
+    var r = color >> 16 &0xff,
+        g = color >> 8 & 0xff,
+        b = color & 0xff,
+        a = (alpha < 0) ? 0 : ((alpha > 1) ? 1 : alpha);
+
+    if (a === 1) {
+        return "rgb(" + r + "," + g + "," + b + ")";
+    } else {
+        return "rgba(" + r + "," + g + "," + b + "," + a + ")";
+    }
+};
