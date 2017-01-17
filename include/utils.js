c2d585a02dc44afc496d85e30e1fa7e4dfa7922f
wangbx <wangbinxin001@126.com>
2017-01-17 20:29:07 +0800

    Implement Chapter3 example codes and extra interesting effects; Add parseColor method to utils.js

--- include/utils.js
+++ include/utils.js
@@ -59,4 +59,21 @@ utils.captureTouch = function (element) {
     }, false);
 
     return touch;
-}
+};
+
+utils.parseColor = function (color, toNumber) {
+    if (toNumber === true) {
+        if (typeof color === 'number') {
+            return (color | 0); //chop off decimal
+        }
+        if (typeof color === 'string' && color[0] === '#') {
+            color = color.slice(1);
+        }
+        return window.parseInt(color, 16);
+    } else {
+        if (typeof color === 'number') {
+            color = '#' + ('00000' + (color | 0).toString(16)).substr(-6); //pad
+        }
+        return color;
+    }
+};
